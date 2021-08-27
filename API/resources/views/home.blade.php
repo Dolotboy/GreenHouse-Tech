b9f3cdf7d4351c6efae25bfb140aca75f57eca75
Dolotboy <maxime-lepage@hotmail.ca>
2021-08-27 14:51:20 -0400

    Start of API

---
+++ API/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>Home</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id='mainContainer'>
+            <a href="{{route('add')}}">
+                <div class='cbtn'>
+                <h2></h2>
+                </div>
+            </a>
+            <a href="{{route('search')}}">
+                <div class='cbtn'>
+                <h2></h2>
+                </div>
+            </a>
+            <a href="{{route('deleteSearch')}}">
+                <div class='cbtn'>
+                <h2></h2>
+                </div>
+            </a>
+        </div>
+    </body>
+</html>
--- API/routes/web.php
+++ API/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ControllerAdd;
+use App\Http\Controllers\ControllerDetail;
+use App\Http\Controllers\ControllerDelete;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +18,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::post('/new', [ControllerAdd::class, 'index'])->name('new');
+
+Route::post('/new/add', [ControllerAdd::class, 'add'])->name('add');
+
+
+Route::post('/search', [ControllerDetail::class, 'index'])->name('search');
+
+Route::get("search/{id}", [ControllerDetail::class, 'search'],function ($id){})->name('detail');
+
+
+Route::post('/delete', [ControllerDelete::class, 'index'])->name('deleteSearch');
+
+Route::get("/delete/{id}", [ControllerDelete::class, 'delete'],function ($id){})->name('delete');
