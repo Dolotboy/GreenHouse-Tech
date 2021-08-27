8d7b539e2c360ea73bd8abc6dc65bdfcbf7fd63c
Francis Lavoie <LavFr1932614@etu.cegepjonquiere.ca>
2021-08-27 15:00:55 -0400

    Added files and updated .gitignore

---
+++ ExporterDonnees/ImportationDonneesExcel/ImportationDonneesExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace ImportationDonneesExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
