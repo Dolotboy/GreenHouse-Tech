16445d933794dd38e52a3d96a91a4acde9d6d7e9
Francis Lavoie <LavFr1932614@etu.cegepjonquiere.ca>
2021-08-30 09:57:57 -0400

    V1 de l'outil d'importation réalisée.

---
+++ OutilImportation/OutilImportation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OutilImportation
+{
+    class Program
+    {
+        static string baseDir = Directory.GetCurrentDirectory().Split(new string[] {"bin"}, StringSplitOptions.None)[0];
+
+        static void Main(string[] args)
+        {
+            List<Vegetal> veggies = new List<Vegetal>();
+            LoadVeggies(veggies);
+            SaveFiles(veggies);
+        }
+
+        static void LoadVeggies(List<Vegetal> veggies)
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(baseDir + "infoleg.xlsx");
+            Excel.Worksheet ws = (Excel.Worksheet)wb.Sheets[1];
+            Excel.Range range = ws.UsedRange;
+
+            for (int i = 1; i < range.Rows.Count; i++)
+            {
+                Vegetal veg = new Vegetal()
+                {
+                    Saison = ConvertCellToString(range.Cells[i, 2]),
+                    TypeVegetal = ConvertCellToString(range.Cells[i, 3]),
+                    NomVegetal = ConvertCellToString(range.Cells[i, 1]),
+                    JoursConservation = ConvertCellToString(range.Cells[i, 4]),
+                    Fonctionnement = ConvertCellToString(range.Cells[i, 5])
+                };
+                veggies.Add(veg);
+            }
+            wb.Close();
+            app.Quit();
+        }
+
+        static string ConvertCellToString(Excel.Range cell)
+        {
+            if (cell.Value == null)
+                return "";
+            return cell.Value.ToString();
+        }
+
+        static void SaveFiles(List<Vegetal> veggies)
+        {
+            StreamWriter writer = new StreamWriter(baseDir + "statements.txt");
+            foreach (Vegetal veg in veggies)
+                writer.WriteLine(veg.ToString());
+            writer.Close();
+        }
+    }
+}
---
+++ OutilImportation/OutilImportation/Vegetal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutilImportation
+{
+    internal class Vegetal
+    {
+        public string Saison;
+        public string TypeVegetal;
+        public string NomVegetal;
+        public string JoursConservation;
+        public string Fonctionnement;
+
+        public override string ToString()
+        {
+            return $"INSERT INTO tblVegetal (saison, typeVegetal, nomVegetal, joursConservation, fonctionnement) " +
+                   $"VALUES ({Saison}, {TypeVegetal},{NomVegetal},{JoursConservation},{Fonctionnement})";
+        }
+    }
+}
