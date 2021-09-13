72bd3abbdda1df66043da5748ea8420e9a166c5b
dufgu1932889 <dufgu1932889@etu.cegepjonquiere.ca>
2021-09-13 13:44:08 -0400

    Push problème vue.js

--- HMI/src/views/Home.vue
+++ HMI/src/views/Home.vue
@@ -1,14 +1,40 @@
 <template>
   <h1>Ceci est la page d'acceuil</h1> 
   <Details></Details>
+  <!-- #####LOOP TO DISPLAY EVERY PLANT
+    <ul>
+      {foreach $plants as $plant}
+      <li>{@plant}</li>
+      {/foreach}
+    </ul>
+  -->
 </template>
 
 <script>
 import Details from '../components/Details.vue'
 export default {
-  components: {
+  name: 'Home',
+  components:{
     Details
-  }
+  }, 
+  data(){
+      return{
+          products : ""
+      }
+  },
+  mounted(){
+      //this.GetAllPlants()
+  },
+  methods :{
+    GetAllPlants(url){
+      $.get(url, function(donnees, status){
+          for(const [key, value] of Object.entries(donnees.results))
+              ships.push(value);
+          if(donnees.next != null)
+              GetAllPlants(donnees.next);
+      })   
+    }
+  }   
 }
 </script>
 
