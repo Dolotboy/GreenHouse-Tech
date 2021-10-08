6c90e434beaee6d936ff2da882c007f70d263f88
alecles <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>
2021-10-08 14:40:00 -0400

    Push Request#044 Test GetVersion

--- API/tests/Unit/GetVersionTest.php
+++ API/tests/Unit/GetVersionTest.php
@@ -5,27 +5,24 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use app\Models\Version;
 use Illuminate\Support\Facades\Http;
-use App\Models\Plant;
 
-
-class PlantTest extends TestCase
+class GetVersionTest extends TestCase
 {
     /**
      * A basic feature test example.
      *
      * @return void
      */
-
-    public function test_Plant()
+    public function test_GetVersion()
     {
         $this->withoutExceptionHandling();
 
-        $data = Plant::find(1);
-        $dataEncode = json_encode($data);
-        $donneApi = Http::acceptJson()->get('http://127.0.0.1:8000/api/search/plant/1');
+        $data = Version::orderBy('idVersion', 'DESC')->get();
+        $dataEncode = json_encode($data[0], JSON_NUMERIC_CHECK);
+        $donneApi = Http::acceptJson()->get('http://127.0.0.1:8000/api/search/last/version');
 
         $this->assertEquals($dataEncode,$donneApi);
-
     }
 }
