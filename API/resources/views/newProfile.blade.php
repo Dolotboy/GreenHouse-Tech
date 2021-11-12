005f87a3acb42be6e66864e1b8212b8b50a02618
Dolotboy <maxime-lepage@hotmail.ca>
2021-11-12 14:03:09 -0500

    Push request#119 show password

--- API/resources/views/newProfile.blade.php
+++ API/resources/views/newProfile.blade.php
@@ -4,7 +4,6 @@
 @parent
 
 <div id='mainContainer'>
-        
         <form action='{{route('addProfile')}}' method='POST'>
             @csrf
 
@@ -13,10 +12,11 @@
                     <div class="row">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 pl-xl-0 pl-lg-0 pl-md-0 border-left m-b-30">
                             <div class="product-details">
-                            <h3>Add your information</h3>
+                                <h3>Add your information</h3>
                                 <div class="border-bottom pb-3 mb-3">
                                     <h2 class="mb-3"> <input type="email" name='email' placeholder="Email" value="" required></h2>
-                                    <h3 class="mb-3"> <input type="text" name='password' placeholder="Password" value="" required></h3>
+                                    <h3 class="mb-3"> <input type="password" id='passInput' name='password' placeholder="Password" value="" required></h3>
+                                    <input type="checkbox" onclick="showPass()">Show Password</input>
                                     <h3 class="mb-3"> <input type="text" name='firstName' placeholder="First Name" value="" required></h3>
                                     <h3 class="mb-3"> <input type="text" name='lastName' placeholder="Last Name" value="" required></h3>   
                                     <h3 class="mb-3"> <input type="text" name='access' placeholder="Access" value="" required></h3>  
@@ -28,5 +28,15 @@
                 </div>
             </div>
         </form>
-    </div>
+</div>
+<script>
+function showPass() {
+  var x = document.getElementById("passInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+</script>
 @endsection
