495cb405a37173eda6b13f84a3a4512908792a46
Francis Lavoie <francis.lavoie79@outlook.com>
2021-12-06 14:24:24 -0500

    Stash pop done.

---
+++ OutilImportation/OutilImportation/Problem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutilImportation
+{
+    public class Problem
+    {
+        public string DatabaseId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Solution { get; set; }
+        public List<int> Plants { get; set; }
+        public string Created_at { get; set; }
+        public string Updated_at { get; set; }
+    }
+}
