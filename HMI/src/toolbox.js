ca104844fe79e7d6b1b70286e03b92a7858f8281
GDuf12 <gui.duf12@gmail.com>
2021-12-14 10:23:30 -0500

    Résolution de conflits

--- HMI/src/toolbox.js
+++ HMI/src/toolbox.js
@@ -2,8 +2,8 @@ exports.isProd = false;
 
 exports.getApiUrl = () =>{
     if(this.isProd)
-        return "http://testenv.apipcst.xyz/api/"; 
-    return "http://localhost:8000/api/";
+        return "http://localhost:8000/api/"; 
+    return "http://testenv.apipcst.xyz/api/";
 }
 
 exports.getScriptUrl = () =>{
